479b60fd4d4c5eea906092c77f544c6949a3602a
cristianfchq <cristian.fchq@gmail.com>
2019-10-09 13:51:32 -0400

    aplicando estilo al texto AQUI y agregando un boton para el scaner

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: HomePage(),
-));
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("QR SCANER",),
+        title: Text(
+          "QR SCANER",
+        ),
       ),
-      body: Center(child: Text("AQUI"),),
+      body: Center(
+        child: Text(
+          "AQUI",
+          style: TextStyle(
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.camera_alt),
+        label: Text("scan qr"),
+        onPressed: (){},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }

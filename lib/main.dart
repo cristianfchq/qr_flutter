902218a49eee68367c79b73ba0d7fcda98976efc
cristianfchq <cristian.fchq@gmail.com>
2019-10-09 13:43:00 -0400

    titulo appBar

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: HomePage(),
+));
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QR SCANER",),
+      ),
+      body: Center(child: Text("AQUI"),),
+    );
+  }
+}
